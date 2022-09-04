e35dabe429f27fecc3632ff01b53e5514f3a13f5
namtran02022003 <tranhuytunam02022003@gmail.com>
2022-09-04 17:33:02 +0700

    code 16 bai

---
+++ src/react1.js
@@ -0,0 +1,292 @@
+import './App.css'
+function React1 () {
+    return (
+        <div>
+        <div className="header">
+          <div className="header-left">
+            <a className="a-header" href="#"> <i className="fa fa-facebook-official" /></a>
+            <a className="a-header" href="#"> <i className="fa fa-instagram" /></a>
+            <a className="a-header" href="#"> <i className="fa fa-snapchat" /></a>
+            <a className="a-header" href="#"> <i className="fa fa-flickr" /></a>
+            <a className="a-header" href="#"> <i className="fa fa-twitter" /></a>
+            <a className="a-header" href="#"> <i className="fa fa-linkedin" /></a>
+          </div>
+          <div className="header-left">
+            <a className="a-header" href="#"> <i className="fa fa-search" /></a>
+          </div>
+        </div>
+        <div className="w3-content">
+          <div className="text-header">
+            <h1 className="style-h1">
+              <b>JANE BLOGLIFE</b>
+            </h1>
+            <h6 style={{fontSize: '16px'}}>Welcome to the blog of
+              <span className="span-top">
+                Jane's world
+              </span>
+            </h6>
+          </div>
+          <div className="image-content-top">
+            <img style={{maxWidth: '100%', height: 'auto'}} src="https://www.w3schools.com/w3images/jane.jpg" width={1600} height={1060} />
+            <div className="text-image-content-top">
+              <h1 style={{color: '#fff'}}>Jane's</h1>
+              <h1 style={{fontSize: '64px', color: '#fff'}}>FASHION BLOG</h1>
+              <h6>
+                <button style={{padding: '12px 24px', backgroundColor: 'rgb(209, 195, 195)', border: 'none'}}>
+                  SUBSRCIBE
+                </button>
+              </h6>
+            </div>
+          </div>
+          <div className="body-content">
+            <div className="menu-60">
+              <div className="menu-60-content">
+                <div className="div-center">
+                  <h3>TITLE HEADING</h3>
+                  <h5>Title description, <span className="opacity">May 2, 2016</span></h5>
+                </div>
+                <div className="image-text-menu-60">
+                  <img src="https://www.w3schools.com/w3images/girl_hat.jpg" alt="Girl Hat" style={{width: '100%'}} className="w3-padding-16" />
+                  <p><strong>More Hats!</strong> I am crazy about hats these days. Some text about this blog
+                    entry.
+                    Fashion fashion and mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl.
+                    Sed mattis nunc id lorem euismod placerat. Vivamus porttitor
+                    magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi
+                    non congue ullam corper. Praesent tincidunt sedtellus ut rutrum. Sed vitae justo
+                    condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.
+                  </p>
+                  <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco.
+                  </p>
+                  <div className="button-left">
+                    <buton className="button-left1">
+                      <i className="fa fa-thumbs-up" />
+                      like
+                    </buton>
+                    <buton className="button-left2">
+                      <p>Replies <span style={{padding: '2px 10px', background: '#fff', color: '#333', marginRight: '2px'}}>1</span>
+                      </p>
+                    </buton>
+                  </div>
+                  <hr />
+                  <div className="div9" style={{marginBottom: '16px'}}>
+                    <div className="image5">
+                      <img src="https://www.w3schools.com/w3images/avatar_smoke.jpg" style={{width: '90px'}} />
+                    </div>
+                    <div className="text5">
+                      <h4>George
+                        <span className="opacity" style={{fontSize: '14px'}}>May 3, 2015, 6:32 PM</span>
+                      </h4>
+                      <p>Great blog post! Following</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <hr style={{opacity: '0.3'}} />
+              <div className="menu-60-content">
+                <div className="div-center">
+                  <h3 style={{fontSize: '24px'}}>TITLE HEADING</h3>
+                  <h5 style={{fontSize: '20px'}}>Title description,<span className="opacity">April 23, 2016</span></h5>
+                </div>
+                <div className="image-text-menu-60">
+                  <img src="https://www.w3schools.com/w3images/man_hat.jpg" alt="Men in Hats" style={{width: '100%'}} className="w3-padding-16" />
+                  <p><strong>Hats!</strong> The trend this summer is hats for men!</p>
+                  <p>Some text about this blog entry. Fashion fashion and mauris neque quam,
+                    fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem
+                    euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at.
+                    Phasellus sed ultricies
+                    mi non congue ullam corper. Praesent tincidunt sedtellus ut rutrum. Sed
+                    vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.
+                  </p>
+                  <div className="button-left">
+                    <buton className="button-left1">
+                      <i className="fa fa-thumbs-up" />
+                      like
+                    </buton>
+                    <buton className="button-left2">
+                      <p>Replies <span style={{padding: '2px 10px', background: '#fff', color: '#333', marginRight: '2px'}}>1</span>
+                      </p>
+                    </buton>
+                  </div>
+                </div>
+              </div>
+              <div className="menu-60-content">
+                <div className="div-center">
+                  <h3 style={{fontSize: '24px'}}>TITLE HEADING</h3>
+                  <h5 style={{fontSize: '20px'}}>Title description,<span className="opacity">April 7, 2016</span></h5>
+                </div>
+                <div className="image-text-menu-60">
+                  <img src="https://www.w3schools.com/w3images/runway.jpg" alt="Men in Hats" style={{width: '100%'}} className="w3-padding-16" />
+                  <p><strong>Dont miss! </strong> The trend this summer is hats for men!</p>
+                  <p>Some text about this blog entry. Fashion fashion and mauris neque quam,
+                    fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem
+                    euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at.
+                    Phasellus sed ultricies
+                    mi non congue ullam corper. Praesent tincidunt sedtellus ut rutrum. Sed
+                    vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.
+                  </p>
+                  <div className="button-left">
+                    <buton className="button-left1">
+                      <i className="fa fa-thumbs-up" />
+                      like
+                    </buton>
+                    <buton className="button-left2">
+                      <p>Replies <span style={{padding: '2px 10px', background: '#fff', color: '#333', marginRight: '2px'}}>1</span>
+                      </p>
+                    </buton>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="menu-40">
+              <div className="menu-40-top">
+                <img src="https://www.w3schools.com/w3images/avatar_girl2.jpg" alt="Jane" style={{width: '100%'}} className="w3-grayscale" />
+                <div className="text-menu-40-top">
+                  <h4 style={{fontSize: '20px'}}> MY NAME</h4>
+                  <p>Just me, myself and I, exploring the universe of uknownment.
+                    I have a heart of love and a interest of lorem ipsum and mauris neque
+                    quam blog. I want to share my world with you.
+                  </p>
+                </div>
+              </div>
+              <hr style={{opacity: '0.1'}} />
+              <div className="menu-40-top">
+                <h4 style={{fontSize: '20px', color: '#fff', padding: '20px 10px'}}> Popular Posts</h4>
+                <div style={{background: '#fff', color: 'black'}} className="text-menu-40-top5">
+                  <div className="w3-padding-16">
+                    <img src="https://www.w3schools.com/w3images/avatar_smoke.jpg" width="50px" className="img-40-small" />
+                    <span style={{fontSize: '19px', lineHeight: '1.5'}}>demin</span>
+                    <br />
+                    <span style={{fontSize: '19px'}}>Sed mattis nunc</span>
+                  </div>
+                </div>
+                <div style={{background: '#fff', color: 'black'}} className="text-menu-40-top5">
+                  <div className="w3-padding-16">
+                    <img src="https://www.w3schools.com/w3images/avatar_smoke.jpg" width="50px" className="img-40-small" />
+                    <span style={{fontSize: '19px', lineHeight: '1.5'}}>demin</span>
+                    <br />
+                    <span style={{fontSize: '19px'}}>Sed mattis nunc</span>
+                  </div>
+                </div>
+                <div style={{background: '#fff', color: 'black'}} className="text-menu-40-top5">
+                  <div className="w3-padding-16">
+                    <img src="https://www.w3schools.com/w3images/avatar_smoke.jpg" width="50px" className="img-40-small" />
+                    <span style={{fontSize: '19px', lineHeight: '1.5'}}>demin</span>
+                    <br />
+                    <span style={{fontSize: '19px'}}>Sed mattis nunc</span>
+                  </div>
+                </div>
+                <div style={{background: '#fff', color: 'black'}} className="text-menu-40-top5">
+                  <div className="w3-padding-16">
+                    <img src="https://www.w3schools.com/w3images/avatar_smoke.jpg" width="50px" className="img-40-small" />
+                    <span style={{fontSize: '19px', lineHeight: '1.5'}}>demin</span>
+                    <br />
+                    <span style={{fontSize: '19px'}}>Sed mattis nunc</span>
+                  </div>
+                </div>
+              </div>
+              <hr style={{opacity: '0.1'}} />
+              <div className="menu-40-top">
+                <div className="text-menu-40-top">
+                  <h4 style={{fontSize: '20px'}}>Advertise</h4>
+                </div>
+                <div className="div-padding">
+                  <div className="div-paddiing-content">
+                    <span className="div-padding-span">
+                      your AD here
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <hr style={{opacity: '0.1'}} />
+              <div className="menu-40-top">
+                <div className="text-menu-40-top">
+                  <h4 style={{fontSize: '20px'}}>tags</h4>
+                </div>
+                <div className="div-padding">
+                  <p>
+                    <span style={{backgroundColor: 'black', color: '#fff'}} className="span-p">Fashion</span>
+                    <span className="span-p">New York</span>
+                    <span className="span-p">London </span>
+                    <span className="span-p">Hats </span>
+                    <span className="span-p">Norway</span>
+                    <span className="span-p">Sweaters</span>
+                    <span className="span-p">Ideas</span>
+                    <span className="span-p">Deals</span>
+                    <span className="span-p">Accessories</span>
+                    <span className="span-p">News</span>
+                    <span className="span-p">Clothing</span>
+                    <span className="span-p">Shopping</span>
+                    <span className="span-p">Jeans</span>
+                    <span className="span-p"> Trends</span>
+                  </p>
+                </div>
+              </div>
+              <hr style={{opacity: '0.1'}} />
+              <div className="menu-40-top">
+                <div className="text-menu-40-top">
+                  <h4 style={{fontSize: '20px'}}>Inspiration</h4>
+                </div>
+                <div className="div-padding">
+                  <div className="left-div-padding">
+                    <p><img src="https://www.w3schools.com/w3images/jeans.jpg" alt="Jeans" style={{width: '100%'}} />
+                    </p>
+                    <p><img src="https://www.w3schools.com/w3images/team1.jpg" alt="Jeans" style={{width: '100%'}} />
+                    </p>
+                  </div>
+                  <div className="left-div-padding">
+                    <p><img src="https://www.w3schools.com/w3images/avatar_hat.jpg" alt="Jeans" style={{width: '100%'}} /></p>
+                    <p><img src="https://www.w3schools.com/w3images/team4.jpg" alt="Jeans" style={{width: '100%'}} />
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <hr style={{opacity: '0.1'}} />
+              <div className="menu-40-top">
+                <div className="text-menu-40-top">
+                  <h4 style={{fontSize: '20px'}}>Follow Me</h4>
+                </div>
+                <div className="div-padding">
+                  <p>
+                    <i style={{fontSize: '24px'}} className="fa n fa-facebook-official " />
+                    <i style={{fontSize: '24px'}} className="fa n fa-instagram " />
+                    <i style={{fontSize: '24px'}} className="fa n fa-snapchat " />
+                    <i style={{fontSize: '24px'}} className="fa n fa-pinterest-p " />
+                    <i style={{fontSize: '24px'}} className="fa n fa-twitter " />
+                    <i style={{fontSize: '24px'}} className="fa n fa-linkedin " />
+                  </p>
+                </div>
+              </div>
+              <hr style={{opacity: '0.1'}} />
+              <div className="menu-40-top">
+                <div className="text-menu-40-top">
+                  <h4 style={{fontSize: '20px'}}>Subscribe</h4>
+                </div>
+                <div className="div-padding">
+                  <p>Enter your e-mail below and get notified on the latest blog posts.
+                  </p><form>
+                    <input style={{width: '100%', padding: '10px'}} type="text" placeholder="enter email" />
+                    <button className="button5">Subscribe</button>
+                  </form>
+                  <p />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="footer">
+          <div className="content-footer">
+            <a href="#" className="afooter">
+              <i className="fa fa-arrow-up " />
+              to the top
+            </a>
+            <p>
+              tran huy tu <a style={{color: '#fff'}} href="#">nam</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    )
+}
+export default React1;
